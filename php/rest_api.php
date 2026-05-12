1dd0f6265788c2ada332d1edba67fae6ef341a3a
Tsjippy <ewaldha@hotmail.com>
2026-05-12 07:47:30 +0200

    changed: code relocation

--- php/rest_api.php
+++ php/rest_api.php
@@ -23,109 +23,6 @@ function allowedRestApiUrls($urls){
 	return $urls;
 }
 
-function getNextMonth(){
-	$bookings	= new Bookings();
-
-	$bookings->forms->getForm($_POST['form-id']);
-
-	$bookings->forms->shortcodeId		= $_POST['shortcode-id'];
-
-	if(isset($_POST['element-id']) && is_numeric($_POST['element-id'])){
-		$element						= $bookings->forms->getElementById($_POST['element-id']);
-	}else{
-		foreach($bookings->forms->formElements as $element){
-			if($element->type == 'booking-selector'){
-				break;
-			}
-		}
-	}
-	$bookings->forms->currentElement	= $element;
-
-	$subjectName	= sanitize_text_field($_POST['subject']);
-	$date			= strtotime($_POST['year'].'-'.$_POST['month'].'-01');
-
-	$months			= [];
-	foreach($bookings->getElementSubjects($element->id) as $subject){
-		if($subject['name'] == $subjectName){
-			if($subject['amount'] > 1){									
-				if(isset($subject['nrtype']) && $subject['nrtype'] == 'letters'){
-					$alphabet = range('A', 'Z');
-					for ($x = 0; $x < $subject['amount']; $x++) {
-						$months[]	= $bookings->monthCalendar($subject['name'], $alphabet[$x], $date);
-					}
-				}elseif(isset($subject['nrtype']) && $subject['nrtype'] == 'custom'){
-					foreach ($subject['rooms'] as $room) {
-						$months[]	= $bookings->monthCalendar($subject['name'], $room, $date);
-					}
-				}else{
-					for ($x = 1; $x <= $subject['amount']; $x++) {
-						$months[]	= $bookings->monthCalendar($subject['name'], $x, $date);
-					}
-				}
-			}else{
-				$months[]	= $bookings->monthCalendar($subject['name'], '', $date);
-			}
-		}
-	}
-
-	/**
-	 * date is the month we are requesting
-	 * the navigator expect the month given to be the first visible month
-	 * So if we are adding a new month the first visible month will be the month before that
-	 */ 
-	if(isset($_POST['type']) && $_POST['type'] == 'prev'){
-		$navDate	= $date;
-	}else{
-		$navDate	= strtotime('-1 month', $date);
-	}
-	$navigator	= $bookings->getNavigator($navDate);
-	$detail		= '';
-	if(!empty($_POST['shortcode-id'])){
-		$detail		= $bookings->detailHtml();
-	}
-
-	if(is_wp_error($detail)){
-		return $detail;
-	}
-
-	if(is_wp_error($navigator)){
-		return $navigator;
-	}
-	
-	return [
-		'months'	=> $months,
-		'navigator'	=> $navigator,
-		'details'	=> $detail
-	];
-}
-
-function approveBooking(){
-	$bookingsObject	= new Bookings();
-
-	$bookingsObject->forms->formData->id	= $_POST['form-id'];
-
-	$bookings    				= $bookingsObject->getBookingsBySubmission($_POST['id']);
-
-	$result						= false;
-	foreach($bookings as $booking){
-		$result	= $bookingsObject->updateBooking($booking, ['pending' => 0]);
-	}
-
-	if(is_wp_error($result)){
-		return $result;
-	}
-
-	return $result;
-}
-
-function removeBooking(){
-	$bookings	= new Bookings();
-
-	$bookings->removeBooking($_POST['id']);
-
-	return 'Booking removed succesfully';
-}
-
 add_action( 'rest_api_init', __NAMESPACE__.'\restapiInit' );
 function restapiInit() {
 	// Next month
@@ -232,6 +129,110 @@ function restapiInit() {
 			)
 		)
 	);
+}
+
+
+function getNextMonth(){
+	$bookings	= new Bookings();
+
+	$bookings->forms->getForm($_POST['form-id']);
+
+	$bookings->forms->shortcodeId		= $_POST['shortcode-id'];
+
+	if(isset($_POST['element-id']) && is_numeric($_POST['element-id'])){
+		$element						= $bookings->forms->getElementById($_POST['element-id']);
+	}else{
+		foreach($bookings->forms->formElements as $element){
+			if($element->type == 'booking-selector'){
+				break;
+			}
+		}
+	}
+	$bookings->forms->currentElement	= $element;
+
+	$subjectName	= sanitize_text_field($_POST['subject']);
+	$date			= strtotime($_POST['year'].'-'.$_POST['month'].'-01');
+
+	$months			= [];
+	foreach($bookings->getElementSubjects($element->id) as $subject){
+		if($subject['name'] == $subjectName){
+			if($subject['amount'] > 1){									
+				if(isset($subject['nrtype']) && $subject['nrtype'] == 'letters'){
+					$alphabet = range('A', 'Z');
+					for ($x = 0; $x < $subject['amount']; $x++) {
+						$months[]	= $bookings->monthCalendar($subject['name'], $alphabet[$x], $date);
+					}
+				}elseif(isset($subject['nrtype']) && $subject['nrtype'] == 'custom'){
+					foreach ($subject['rooms'] as $room) {
+						$months[]	= $bookings->monthCalendar($subject['name'], $room, $date);
+					}
+				}else{
+					for ($x = 1; $x <= $subject['amount']; $x++) {
+						$months[]	= $bookings->monthCalendar($subject['name'], $x, $date);
+					}
+				}
+			}else{
+				$months[]	= $bookings->monthCalendar($subject['name'], '', $date);
+			}
+		}
+	}
+
+	/**
+	 * date is the month we are requesting
+	 * the navigator expect the month given to be the first visible month
+	 * So if we are adding a new month the first visible month will be the month before that
+	 */ 
+	if(isset($_POST['type']) && $_POST['type'] == 'prev'){
+		$navDate	= $date;
+	}else{
+		$navDate	= strtotime('-1 month', $date);
+	}
+	$navigator	= $bookings->getNavigator($navDate);
+	$detail		= '';
+	if(!empty($_POST['shortcode-id'])){
+		$detail		= $bookings->detailHtml();
+	}
+
+	if(is_wp_error($detail)){
+		return $detail;
+	}
+
+	if(is_wp_error($navigator)){
+		return $navigator;
+	}
+	
+	return [
+		'months'	=> $months,
+		'navigator'	=> $navigator,
+		'details'	=> $detail
+	];
+}
+
+function approveBooking(){
+	$bookingsObject	= new Bookings();
+
+	$bookingsObject->forms->formData->id	= $_POST['form-id'];
+
+	$bookings    				= $bookingsObject->getBookingsBySubmission($_POST['id']);
+
+	$result						= false;
+	foreach($bookings as $booking){
+		$result	= $bookingsObject->updateBooking($booking, ['pending' => 0]);
+	}
+
+	if(is_wp_error($result)){
+		return $result;
+	}
+
+	return $result;
+}
+
+function removeBooking(){
+	$bookings	= new Bookings();
+
+	$bookings->removeBooking($_POST['id']);
+
+	return 'Booking removed succesfully';
 }
 
 function loadPost(){
